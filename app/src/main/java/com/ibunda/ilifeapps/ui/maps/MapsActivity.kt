c98940c4b1d91862002aa9f5628e8e9d7f7c0d4e
Tangguh Tri Prasetyo <tangguhtriprasetyo@gmail.com>
2021-08-15 23:52:52 +0700

    Autocomplete Places

--- app/src/main/java/com/ibunda/ilifeapps/ui/maps/MapsActivity.kt
+++ app/src/main/java/com/ibunda/ilifeapps/ui/maps/MapsActivity.kt
@@ -1,6 +1,11 @@
 package com.ibunda.ilifeapps.ui.maps
 
+import android.app.Activity
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -8,30 +13,62 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.model.TypeFilter
+import com.google.android.libraries.places.widget.Autocomplete
+import com.google.android.libraries.places.widget.AutocompleteActivity
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
+import com.ibunda.ilifeapps.BuildConfig.MAPS_API_KEY
 import com.ibunda.ilifeapps.R
+import com.ibunda.ilifeapps.databinding.ActivityMapsBinding
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    private val getResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            when (result.resultCode) {
+                Activity.RESULT_OK -> {
+                    val place = result.data?.let { Autocomplete.getPlaceFromIntent(it) }
+                    Log.i(TAG, "Place: ${place?.name}, ${place?.id}")
+                }
+                AutocompleteActivity.RESULT_ERROR -> {
+                    // TODO: Handle the error.
+                    val status = result.data?.let { Autocomplete.getStatusFromIntent(it) }
+                    Log.i(TAG, status?.statusMessage.toString())
+                }
+                Activity.RESULT_CANCELED -> {
+                    // The user canceled the operation.
+                }
+
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_maps)
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Initialize the SDK
+        Places.initialize(applicationContext, MAPS_API_KEY)
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        // Create a new PlacesClient instance
+        val placesClient = Places.createClient(this)
+
+
+        binding.searchLocation.setOnClickListener {
+            openPlacesApi()
+        }
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
@@ -39,5 +76,14 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    private fun openPlacesApi() {
+        val fields = listOf(Place.Field.ID, Place.Field.NAME)
+        val intent = Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY, fields)
+            .setTypeFilter(TypeFilter.ADDRESS)
+            .build(this)
+        getResult.launch(intent)
+
     }
 }
